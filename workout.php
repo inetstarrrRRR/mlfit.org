901c7a2e5eb6bbf1b8cbc2969418e880f3790a2b
inetstarrrRRR <121507376+inetstarrrRRR@users.noreply.github.com>
2025-07-18 12:06:27 +0200

    Update workout.php
    update for website

--- workout.php
+++ workout.php
@@ -1,74 +1,12 @@
-<!DOCTYPE html>
-<html>
-
-<head>
-    <title>Workout</title>
-    <style>
-        body {
-            background-color: powderblue;
-        }
-
-        table,
-        th,
-        td {
-            border: 1px solid black;
-        }
-    </style>
-
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-
-
-</head>
-
-<body>
-    <?php
-    session_start();
+<?php session_start();
     //echo '<p>Hello World</p>';
     //echo $_SESSION['id'];
-    $date = date('Y-m-d');
-
-    echo "    <form method='post' action='?add_workout=1'>
-    <table style='width:100%'>
-    <tr>
-    <td>
-    <h1 style='text-align:center'>Add Workout:</h1>
-        <input type='date' name='datepicker' id='datepicker' value='$date' style = 'font-size:20px;
-           margin: auto;
-        display: block;'/>
-        <input type='submit' name='Add_Workout' value='Go' style = 'font-size:20px;
-        padding: 25px 25px 25px 25px;
-        margin: auto;
-        display: block;'>  <br>
-        </td>
-        </tr>
-        <tr>
-        <td>
-            <h1 style='text-align:center'>View/Edit Workouts:</h1>
-        <input type='submit' name='View_Workout' value='Go' style = 'font-size:20px;
-        padding: 25px 25px 25px 25px;
-        margin: auto;
-        display: block;;'> <br>
-        </td>
-        </tr>
-        <tr>
-        <td>
-        <h1 style='text-align:center'>Delete Workouts:</h1>
-        <input type='submit' name='delete_Workout' value='Go' style = 'font-size:20px;
-        padding: 25px 25px 25px 25px;
-        margin: auto;
-        display: block;;'>
-        </td>
-        <tr>
-    
-    </table>
-    </form>"
-    ;
     if (isset($_GET['add_workout'])) {
         if ($_POST["Add_Workout"]) {
-            $servername = "localhost";
-            $username = "root";
+            $servername = "db5018212067.hosting-data.io";
+            $username = "dbu3658664";
             $password = "";
-            $dbname = "workout_site";
+            $dbname = "dbs14428786";
             $workout_Date = $_POST['datepicker'];
             //echo $workout_Date;
             //echo nl2br("\n");
@@ -81,7 +19,7 @@
             $sql = "INSERT INTO Workouts (Date, User_ID)
         VALUES ('$workout_Date', $user_id);";
             //echo $sql;
-            echo nl2br("\n");
+            //echo nl2br("\n");
             if (mysqli_query($conn, $sql)) {
                 //echo "New record created successfully";
                 // Check connection
@@ -115,7 +53,60 @@
         }
     }
     ?>
-</body>
-
+<html>
+<head>
+    <title>Workout</title>
+    <style>
+        body {
+            background-color: powderblue;
+        }
+        table,
+        th,
+        td {
+            border: 1px solid black;
+        }
+      a:link, a:visited {
+  background-color: buttonface;
+  color: black;
+  padding: 25px 25px 25px 25px;;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+        font-size:20px;
+        margin: auto;
+        display: block;
+}
+    </style>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+</head>
+<body>
+<form method='post' action='?add_workout=1'>
+    <table style='width:100%'>
+    <tr>
+    <td>
+    <h1 style='text-align:center'>Add Workout:</h1>
+        <input type='date' name='datepicker' id='datepicker'  style = 'font-size:20px;
+           margin: auto;
+        display: block;'/>
+        <input type='submit' name='Add_Workout' value='Go' style = 'font-size:20px;
+        padding: 25px 25px 25px 25px;
+        margin: auto;
+        display: block;'>  <br>
+        </td>
+        </tr>
+        <tr>
+        <td>
+        <a href="view_workouts.php">View/Edit Workouts</a> <br>
+        </td>
+        </tr>
+        <tr>
+        <td>
+        <a href="delete_Workout.php">Delete Workouts</a> <br>
+        </td>
+        <tr>
+    
+    </table>
+    </form>"
+  </body>
 
 </html>
