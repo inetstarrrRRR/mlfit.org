e5a94de58ab53658332f5c25d463d7d0ac8a53ac
inetstarrrRRR <121507376+inetstarrrRRR@users.noreply.github.com>
2025-07-18 12:04:01 +0200

    Update delete_set.php
    update für Website

--- delete_set.php
+++ delete_set.php
@@ -1,13 +1,31 @@
+<?php session_start();
+ 	$servername = "db5018212067.hosting-data.io";
+    $username = "dbu3658664";
+    $password = "";
+    $dbname = "dbs14428786";
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+    if (isset($_POST['delete'])) {
+        $set_to_delete = $_POST['delete'];
+        $sql = "DELETE FROM Sets WHERE ID = '$set_to_delete';";
+        if (mysqli_query($conn, $sql)) {
+            header(header: 'Location: sets.php');
+        } else {
+            echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+        }
+    }
+    mysqli_close($conn);
+    ?>
 <!DOCTYPE html>
 <html>
-
 <head>
     <title>Delete Set</title>
     <style>
         body {
             background-color: powderblue;
         }
-
         table,
         th,
         td {
@@ -15,37 +33,8 @@
         }
     </style>
 </head>
-
 <body>
-    <?php
-    session_start();
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "workout_site";
-    $conn = mysqli_connect($servername, $username, $password, $dbname);
-    if (!$conn) {
-        die("Connection failed: " . mysqli_connect_error());
-    }
 
-    if (isset($_POST['delete'])) {
-        $set_to_delete = $_POST['delete'];
-        echo $set_to_delete;
-        echo $_SESSION['selected_workout_id'];
-        $sql = "DELETE FROM Sets WHERE ID = '$set_to_delete';";
-        echo $sql;
-
-        if (mysqli_query($conn, $sql)) {
-            echo "SET Deleted";
-            sleep(1);
-            header(header: 'Location: sets.php');
-        } else {
-            echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-        }
-    }
-
-    mysqli_close($conn);
-    ?>
 </body>
 
 </html>
