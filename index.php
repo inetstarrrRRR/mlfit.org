b1881490ff800c782cced19b790f864b717e3804
inetstarrrRRR <121507376+inetstarrrRRR@users.noreply.github.com>
2025-07-25 06:56:06 +0200

    Update index.php
    no more sql injection

--- index.php
+++ index.php
@@ -1,48 +1,34 @@
 <?php session_start();
- 	$servername = "db5018212067.hosting-data.io";
-    $username = "dbu3658664";
-    $password = "";
-    $dbname = "dbs14428786";
+$string = file_get_contents("dbconf.ini");
+$json_a = json_decode($string);
+$servername = $json_a->servername;
+$username = $json_a->username;
+$password = $json_a->password;
+$dbname = $json_a->dbname;
+if (isset($_GET['login'])) {
+    $user_selected = $_POST['name'];
     // Create connection
     $conn = mysqli_connect($servername, $username, $password, $dbname);
     // Check connection
     if (!$conn) {
         die("Connection failed: " . mysqli_connect_error());
     }
-    $sql = "SELECT id, username FROM users";
-    $result = mysqli_query($conn, $sql);
+    //$sql = "SELECT id, username FROM users WHERE Username = '$user_selected'";
+    $query = "SELECT id, username FROM users WHERE Username = ?";
+    $result = mysqli_execute_query($conn, $query, [$user_selected]);
     if (mysqli_num_rows($result) > 0) {
-        // output data of each row
         while ($row = mysqli_fetch_assoc($result)) {
-            //echo "id: " . $row["id"] . " - Name: " . $row["username"] . "<br>";
+            $_SESSION['loggedin'] = True;
+            $_SESSION['id'] = $row["id"];
+            $_SESSION['username'] = $row["username"];
         }
+        header(header: 'Location: workout.php');
     } else {
-        echo "0 results";
+        echo "user not found";
     }
     mysqli_close($conn);
-    if (isset($_GET['login'])) {
-        $user_selected = $_POST['name'];
-        // Create connection
-        $conn = mysqli_connect($servername, $username, $password, $dbname);
-        // Check connection
-        if (!$conn) {
-            die("Connection failed: " . mysqli_connect_error());
-        }
-        $sql = "SELECT id, username FROM users WHERE Username = '$user_selected'";
-        $result = mysqli_query($conn, $sql);
-        if (mysqli_num_rows($result) > 0) {
-            while ($row = mysqli_fetch_assoc($result)) {
-                $_SESSION['loggedin'] = True;
-                $_SESSION['id'] = $row["id"];
-                $_SESSION['username'] = $row["username"];
-            }
-            header(header: 'Location: workout.php');
-        } else {
-            echo "user not found";
-        }
-        mysqli_close($conn);
-    }
-    ?>
+}
+?>
 <!DOCTYPE html>
 <html>
 
@@ -59,12 +45,12 @@
 </head>
 
 <body>
-  <h2>
-    <form method="post" action="?login=1">
-        Name: <input type="text" name="name">
-        <input type="submit" name="submit" value="Submit">
-    </form>
-  </h2>
+    <h2>
+        <form method="post" action="?login=1">
+            Name: <input style="height:60px;font-size:14pt;" type="text" name="name">
+            <input style="height:60px;font-size:14pt;" type="submit" name="submit" value="Submit">
+        </form>
+    </h2>
 </body>
 
 </html>
