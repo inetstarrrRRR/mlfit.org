8c57de03fc6ce681f070b62d4027df47cf005534
inetstarrrRRR <121507376+inetstarrrRRR@users.noreply.github.com>
2025-07-25 06:56:53 +0200

    Update view_workout.php
    no more sql injection

--- view_workout.php
+++ view_workout.php
@@ -1,26 +1,27 @@
 <?php session_start();
-    //echo '<p>Hello World</p>';
-    //echo $_SESSION['id'];
-    if (isset($_GET['edit_workout'])) {
-        foreach ($_POST as $name => $val) {
-            $workout_id_to_Edit = $name;
-        }
-        $_SESSION['selected_workout_id'] = $workout_id_to_Edit;
-        header(header: 'Location: sets.php');
-    } else {
-          $servername = "db5018212067.hosting-data.io";
-          $username = "dbu3658664";
-          $password = "";
-          $dbname = "dbs14428786";
-        $user_id = $_SESSION['id'];
-        //echo $user_id;
-        echo "<h1 style='text-align:center'>Workouts:</h1>";
-        $conn = mysqli_connect($servername, $username, $password, $dbname);
-        // Check connection
-        if (!$conn) {
-            die("Connection failed: " . mysqli_connect_error());
-        }
-        $sql = "Select Workouts.Date, 
+if (isset($_GET['edit_workout'])) {
+    foreach ($_POST as $name => $val) {
+        $workout_id_to_Edit = $name;
+    }
+    $_SESSION['selected_workout_id'] = $workout_id_to_Edit;
+    header(header: 'Location: sets.php');
+} else {
+    $string = file_get_contents("dbconf.ini");
+    $json_a = json_decode($string);
+    $servername = $json_a->servername;
+    $username = $json_a->username;
+    $password = $json_a->password;
+    $dbname = $json_a->dbname;
+    $user_id = $_SESSION['id'];
+    //echo $user_id;
+    echo "<h1 style='text-align:center'>Workouts:</h1>";
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
+    // Check connection
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+    /*
+    $sql = "Select Workouts.Date, 
     Workouts.ID, 
     Sets.Reps, 
     Sets.Weight, 
@@ -30,38 +31,45 @@
     INNER JOIN Sets ON Workouts.ID = Sets.Workout_ID
     INNER JOIN Exercises ON Sets.Exercise_ID = Exercises.ID
     WHERE Workouts.User_ID = '$user_id' ORDER BY Workouts.ID DESC;";
-        //echo $sql;
-        $result = mysqli_query($conn, $sql);
-        //var_dump($result);
-        $i = 0;
-        if (mysqli_num_rows($result) > 0) {
-            while ($row = mysqli_fetch_assoc($result)) {
-                if ($id != $row['ID']) {
-                    $id = $row['ID'];
-                    if ($sets_from_workout != NULL) {
-                        $workouts[] = $sets_from_workout;
-                        $sets_from_workout = [];
-                    }
-                }
-                $i++;
-                $sets_from_workout[] = $row;
-                if ($result->num_rows == $i) {
+    */
+    $query = "Select Workouts.Date, 
+    Workouts.ID, 
+    Sets.Reps, 
+    Sets.Weight, 
+    Sets.Volume, 
+    Exercises.Name
+    From Workouts
+    INNER JOIN Sets ON Workouts.ID = Sets.Workout_ID
+    INNER JOIN Exercises ON Sets.Exercise_ID = Exercises.ID
+    WHERE Workouts.User_ID = ? ORDER BY Workouts.ID DESC;";
+    //echo $sql;
+    $result = mysqli_execute_query($conn, $query, [$user_id]);
+    //var_dump($result);
+    $i = 0;
+    if (mysqli_num_rows($result) > 0) {
+        while ($row = mysqli_fetch_assoc($result)) {
+            if ($id != $row['ID']) {
+                $id = $row['ID'];
+                if ($sets_from_workout != NULL) {
                     $workouts[] = $sets_from_workout;
+                    $sets_from_workout = [];
                 }
             }
-            //var_dump($workouts);
-    
-            foreach ($workouts as $workout) {
-                //var_dump($workout);
-                //$Workoutdatetext = "Workout: " . $workout[0]['Date'] . " (ID" . $workout[0]['ID'] . ")";
-                $Workoutdatetext = $workout[0]['Date'] . " (ID" . $workout[0]['ID'] . ")";
-                echo "$Workoutdatetext";
-                $selected_workout_id = $workout[0]['ID'];
-                $Button_for_Edit = "<form method='post' action='?edit_workout=1' style='display: inline;'>   
+            $i++;
+            $sets_from_workout[] = $row;
+            if ($result->num_rows == $i) {
+                $workouts[] = $sets_from_workout;
+            }
+        }
+        foreach ($workouts as $workout) {
+            $Workoutdatetext = $workout[0]['Date'] . " (ID" . $workout[0]['ID'] . ")";
+            echo "$Workoutdatetext";
+            $selected_workout_id = $workout[0]['ID'];
+            $Button_for_Edit = "<form method='post' action='?edit_workout=1' style='display: inline;'>   
             <input type='submit' name='$selected_workout_id' value='Edit_Workout'> <br>
         </form>";
-                echo "$Button_for_Edit";
-                $table = "
+            echo "$Button_for_Edit";
+            $table = "
             <table style='width:100%'>
             <tr>
                 <th style='width:70%'>Name</th>
@@ -69,38 +77,7 @@
                 <th style='width:10%'>Weight</th>
                 <th style='width:10%'>Volume</th>
             </tr>";
-                foreach ($workout as $set) {
-                    $table = $table . "
-                               <tr>
-                               ";
-                    $table = $table . "<td>" . $set['Name'] . "</td>";
-                    $table = $table . "<td>" . $set['Reps'] . "</td>";
-                    $table = $table . "<td>" . $set['Weight'] . "</td>";
-                    $table = $table . "<td>" . $set['Volume'] . "</td>";
-                    $table = $table . "
-                              </tr>";
-
-                }
-                $table = $table . "
-                          </table>";
-                echo $table;
-                echo "<br>";
-            }
-
-            /*
-            foreach ($sets_from_workout as $set) {
-                if ($id != $set['ID']) {
-                    $id = $set['ID'];
-                    $table = "
-                    <table style='width:100%'>
-                    <tr>
-                        <th>Name</th>
-                        <th>Reps</th>
-                        <th>Weight</th>
-                        <th>Volume</th>
-                    </tr>";
-                    $new_table = True;
-                }
+            foreach ($workout as $set) {
                 $table = $table . "
                                <tr>
                                ";
@@ -108,21 +85,20 @@
                 $table = $table . "<td>" . $set['Reps'] . "</td>";
                 $table = $table . "<td>" . $set['Weight'] . "</td>";
                 $table = $table . "<td>" . $set['Volume'] . "</td>";
-                $table = $table . "<td>" . $set['ID'] . "</td>";
                 $table = $table . "
                               </tr>";
-
             }
             $table = $table . "
                           </table>";
-            echo $table;*/
-        } else {
-            echo "0 results";
+            echo $table;
+            echo "<br>";
         }
-        mysqli_close($conn);
+    } else {
+        echo "0 results";
     }
-    ?>
-
+    mysqli_close($conn);
+}
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -139,10 +115,7 @@
     </style>
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 </head>
-
 <body>
-
+    <a href="workouts.php">Back</a> <br>
 </body>
-
-
 </html>
